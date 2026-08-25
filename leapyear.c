993346cb809e239f9222af6b48719b51ce608168
RiddhimaNegi <riddhima.32013@stu.upes.ac.in>
2026-08-25 23:37:32 +0530

    Refactor leap year calculation logic
    Updated leap year logic to include century and quadricentennial rules.

--- leapyear.c
+++ leapyear.c
@@ -3,10 +3,14 @@ int main(){
 int x;
 printf("enter year in question:");
 scanf("%d", &x);
-if (x % 4 ==0){
-printf("%d is a leap year", x);
-} else {
- printf(" %d is not a leap year", x);
-}
+ if (year % 4 == 0) {
+        printf("%d is a leap year.\n", year);
+   } else if (year % 100 == 0) {
+        printf("%d is not a leap year.\n", year);
+   } else if (year % 400 == 0) {
+        printf("%d is a leap year.\n", year);
+   } else {
+        printf("%d is not a leap year.\n", year);
+   }
 return 0;
 }
