b46be9b90e19baa94ee08490b5d80a7d6ea5db5b
RiddhimaNegi <riddhima.32013@stu.upes.ac.in>
2026-09-15 00:13:17 +0530

    Add files via upload

---
+++ interest.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+int p,r,t,s,n,c;
+printf("enter principle amount");
+scanf("%d", &p);
+printf("\n enter rate of interest");
+scanf("%d", &r);
+printf("\n enter time in years");
+scanf("%d", &t);
+s = (p*r*t/100); 
+printf("the simple interest is %d", s);
+printf("\n enter number of times the principle is compounded anually");
+c = 
+printf("\n the compound interest is %d",c);
+return 0;
+}
